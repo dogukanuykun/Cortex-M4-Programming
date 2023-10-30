96d7bf0c7d5901febaacf23b128ff8d1cbce83e1
Dogukan UYKUN <uykundogukan@gmail.com>
2023-10-30 12:10:15 +0300

    Global tick count

--- task_schedular/Inc/main.h
+++ task_schedular/Inc/main.h
@@ -9,7 +9,7 @@
 #define MAIN_H_
 
 
-#define MAX_TASKS 4
+#define MAX_TASKS 5
 
 /* some stack memory calculations */
 #define SIZE_TASK_STACK 1024U
@@ -18,11 +18,12 @@
 #define SIZE_SRAM ( (128) * (1024))
 #define SRAM_END ((SRAM_START) + (SIZE_SRAM))
 
-#define T1_STACK_START SRAM_END
-#define T2_STACK_START ((SRAM_END) - (SIZE_TASK_STACK))
-#define T3_STACK_START ((SRAM_END) - (2 * SIZE_TASK_STACK))
-#define T4_STACK_START ((SRAM_END) - (3 * SIZE_TASK_STACK))
-#define SCHED_STACK_START ((SRAM_END) - (4 * SIZE_SCHED_STACK))
+#define T1_STACK_START 		SRAM_END
+#define T2_STACK_START 		((SRAM_END) - (SIZE_TASK_STACK))
+#define T3_STACK_START 		((SRAM_END) - (2 * SIZE_TASK_STACK))
+#define T4_STACK_START 		((SRAM_END) - (3 * SIZE_TASK_STACK))
+#define IDLE_STACK_START 	((SRAM_END) - (4 * SIZE_TASK_STACK))
+#define SCHED_STACK_START 	((SRAM_END) - (5 * SIZE_SCHED_STACK))
 
 #define TICK_HZ 			1000U
 #define HSI_CLOCK			16000000U
--- task_schedular/Src/main.c
+++ task_schedular/Src/main.c
@@ -35,10 +35,11 @@ uint32_t get_psp_value(void);
 uint32_t get_psp_value(void);
 void update_next_task();
 
+void task_delay(uint32_t tick_count);
 
 
-
-uint8_t current_task = 0; // task1 is running
+uint8_t current_task = 1; // task1 is running
+uint32_t g_tick_count = 0;
 
 typedef struct
 {
@@ -68,6 +69,10 @@ int main(void)
 	task1_handler();
     /* Loop forever */
 	for(;;);
+}
+
+void idle_task(void){
+	while(1);
 }
 
 void task1_handler(void){
@@ -140,16 +145,19 @@ void init_tasks_stack(void){
 	user_tasks[1].current_state = TASK_RUNNING_STATE;
 	user_tasks[2].current_state = TASK_RUNNING_STATE;
 	user_tasks[3].current_state = TASK_RUNNING_STATE;
+	user_tasks[4].current_state = TASK_RUNNING_STATE;
 
-	user_tasks[0].psp_value = T1_STACK_START;
-	user_tasks[1].psp_value = T2_STACK_START;
-	user_tasks[2].psp_value = T3_STACK_START;
-	user_tasks[3].psp_value = T4_STACK_START;
+	user_tasks[0].psp_value = IDLE_STACK_START;
+	user_tasks[1].psp_value = T1_STACK_START;
+	user_tasks[2].psp_value = T2_STACK_START;
+	user_tasks[3].psp_value = T3_STACK_START;
+	user_tasks[4].psp_value = T4_STACK_START;
 
-	user_tasks[0].task_handler = task1_handler;
-	user_tasks[1].task_handler = task2_handler;
-	user_tasks[2].task_handler = task3_handler;
-	user_tasks[3].task_handler = task4_handler;
+	user_tasks[0].task_handler = idle_task;
+	user_tasks[1].task_handler = task1_handler;
+	user_tasks[2].task_handler = task2_handler;
+	user_tasks[3].task_handler = task3_handler;
+	user_tasks[4].task_handler = task4_handler;
 
 	uint32_t *pPSP;
 	for (int i = 0; i < MAX_TASKS; i++) {
@@ -215,7 +223,10 @@ __attribute__((naked)) void switch_sp_to_psp(void){
 
 }
 
-
+void task_delay(uint32_t tick_count){
+	user_tasks[current_task].block_count = g_tick_count + tick_count;
+	user_tasks[current_task].current_state = TASK_BLOCKED_STATE;
+}
 
 __attribute__((naked)) void SysTick_Handler(void){
 	//Save the context of current task
